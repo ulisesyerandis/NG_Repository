f17b316fbaa7bd8aaa3fa269148774b43c672db5
unknown <uyerandisr@gmail.com>
2023-12-07 07:05:00 -0500

    trying to show a table

---
+++ app/student/student.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.scss'
+})
+export class StudentComponent {
+
+}
